70112f828ffe798f25c15f4b739eba5d6ffa9718
evanfisher <evankfisher@gmail.com>
2024-04-13 13:57:48 -0700

    nice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,96 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { io } from "socket.io-client";
 
 export default function Home() {
+  const [socket, setSocket] = useState<any>(null);
+  const [color, setColor] = useState(false);
+
+  useEffect(() => {
+    if (socket) {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true, video: false })
+        .then((stream) => {
+          console.log("streaming");
+          // Handle the stream here
+          const audioContext = new AudioContext();
+          const source = audioContext.createMediaStreamSource(stream);
+          const processor = audioContext.createScriptProcessor(1024, 1, 1);
+
+          source.connect(processor);
+          processor.connect(audioContext.destination);
+
+          processor.onaudioprocess = function (e) {
+            // console.log("SENDING AUDIO");
+            // Convert audio to a suitable format here, e.g., PCM
+            const input = e.inputBuffer.getChannelData(0);
+            // You could potentially compress or convert this data before sending
+            // console.log("EMITTING AUDIO");
+            if (socket.connected) {
+              console.log("Audio buffer size:", input.length);
+              socket.emit("send_audio", input.buffer);
+            } else {
+              console.log("socket not connected");
+            }
+          };
+        })
+        .catch((error) => {
+          console.error("Error accessing media devices.", error);
+        });
+    }
+
+    return () => {
+      socket?.disconnect();
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    const socket = io("https://sellme.onrender.com");
+
+    socket.on("connect", () => {
+      console.log("Connected:", socket.connected); // true
+      console.log(socket.id);
+    });
+
+    socket.on("receive_audio", function (data) {
+      console.log("playing audio");
+      playAudio(data);
+    });
+
+    setSocket(socket);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  function playAudio(audioData: any) {
+    const audioContext = new AudioContext();
+    const buffer = new Float32Array(audioData); // Assuming audioData is a Float32Array buffer
+    const audioBuffer = audioContext.createBuffer(
+      1,
+      buffer.length,
+      audioContext.sampleRate
+    );
+
+    audioBuffer.copyToChannel(buffer, 0);
+
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(audioContext.destination);
+    source.start();
+  }
+
+  function handlePress() {
+    setColor(!color);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+          Get started by editiig&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
@@ -27,17 +112,19 @@ export default function Home() {
           </a>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <button onClick={handlePress}>Hello</button>
+      {color && (
+        <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+      )}
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
