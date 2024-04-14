d3c18309b5b4d32d90da5ffd0577138380696cdb
evanfisher <evankfisher@gmail.com>
2024-04-13 17:44:13 -0700

    noice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import { io } from "socket.io-client";
 
 export default function Home() {
-  const [socket, setSocket] = useState<any>(null);
-  const [color, setColor] = useState(false);
+  const socket = useRef<any>(null);
+  // const [socket, setSocket] = useState<any>(null);
+  const [connect, setConnect] = useState(false);
+  const [userId, setUserId] = useState("");
 
   useEffect(() => {
-    if (socket) {
+    console.log(socket);
+    console.log(socket.current?.connected, connect);
+    if (socket?.current?.connected && connect) {
+      console.log("CONNECTING AUDIO");
+      // console.log(socket);
       navigator.mediaDevices
         .getUserMedia({ audio: true, video: false })
         .then((stream) => {
@@ -27,9 +33,9 @@ export default function Home() {
             const input = e.inputBuffer.getChannelData(0);
             // You could potentially compress or convert this data before sending
             // console.log("EMITTING AUDIO");
-            if (socket.connected) {
+            if (socket?.current?.connected) {
               console.log("Audio buffer size:", input.length);
-              socket.emit("send_audio", input.buffer);
+              socket?.current?.emit("send_audio", input.buffer);
             } else {
               console.log("socket not connected");
             }
@@ -39,31 +45,36 @@ export default function Home() {
           console.error("Error accessing media devices.", error);
         });
     }
-
-    return () => {
-      socket?.disconnect();
-    };
-  }, [socket]);
+  }, [socket, connect]);
 
   useEffect(() => {
-    const socket = io("https://sellme.onrender.com");
-
-    socket.on("connect", () => {
-      console.log("Connected:", socket.connected); // true
-      console.log(socket.id);
-    });
-
-    socket.on("receive_audio", function (data) {
-      console.log("playing audio");
-      playAudio(data);
-    });
-
-    setSocket(socket);
-
     return () => {
-      socket.disconnect();
+      socket?.current?.disconnect();
     };
   }, []);
+
+  // useEffect(() => {
+  //   // const socket = io("https://sellme.onrender.com");
+  //   socket.current = io("http://localhost:3001");
+
+  //   socket?.current?.on("connect", () => {
+  //     console.log("Connected:", socket.current.connected); // true
+  //     console.log(socket.current.id);
+  //   });
+
+  //   socket?.current?.emit("register", userId);
+
+  //   socket?.current?.on("receive_audio", function (data: any) {
+  //     console.log("playing audio");
+  //     playAudio(data);
+  //   });
+
+  //   // setSocket(socket);
+
+  //   return () => {
+  //     socket?.current?.disconnect();
+  //   };
+  // }, []);
 
   function playAudio(audioData: any) {
     const audioContext = new AudioContext();
@@ -83,37 +94,31 @@ export default function Home() {
   }
 
   function handlePress() {
-    setColor(!color);
+    socket.current = io("http://localhost:3001");
+
+    socket?.current?.on("connect", () => {
+      console.log("Connected:", socket.current.connected); // true
+      console.log(socket.current.id);
+    });
+
+    socket?.current?.emit("register", userId);
+
+    socket?.current?.on("receive_audio", function (data: any) {
+      console.log("playing audio");
+      playAudio(data);
+    });
+    setTimeout(() => setConnect(true), 3000);
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editiig&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-      <button onClick={handlePress}>Hello</button>
-      {color && (
+      <input
+        onChange={(e) => setUserId(e.target.value)}
+        value={userId}
+        className="bg-sky-400"
+      />
+      <button onClick={handlePress}>Connect</button>
+      {/* {color && (
         <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -124,77 +129,7 @@ export default function Home() {
             priority
           />
         </div>
-      )}
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )} */}
     </main>
   );
 }
